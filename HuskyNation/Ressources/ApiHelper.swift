9dcf0f168af234e7c9d22b27edde094d924a8981
Alexander Philipp <63378944+AlexanderPhilipp@users.noreply.github.com>
2021-12-11 11:54:05 -0500

    Add basic navigation and ApiHelper class

---
+++ HuskyNation/Ressources/ApiHelper.swift
@@ -0,0 +1,20 @@
+//
+//  ApiHelper.swift
+//  HuskyNation
+//
+//  Created by Alexander Philipp on 12/11/21.
+//
+
+
+//Class for interacting with the Sky API
+//
+//Request data by awaiting desired function
+//Initialize with the url of the Sky API Endpoint
+
+class ApiHelper{
+    let apiEndpoint : String;
+    
+    init(endpoint : String){
+        apiEndpoint = endpoint;
+    }
+}
---
+++ HuskyNation/View Controllers/HomePageView.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HuskyNation
+//
+//  Created by Alexander Philipp on 12/8/21.
+//
+
+import UIKit
+
+class HomePageView : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
